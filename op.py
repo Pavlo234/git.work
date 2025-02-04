0889292d026dd4359d95f529a2b148647c6fc609
Pavlo234 <ahterovp@gmail.com>
2025-02-04 15:30:33 +0200

    pspatsg

--- op.py
+++ op.py
@@ -1,4 +1,13 @@
 def namber(a,b):
     return a + b
 
-p = 90
+def get_los_angeles_points(results):
+    los_angeles_result = 0
+    for name, score in results:
+        if name.startswith('Los Angeles '):
+            a = name.split()
+            if len(a) == 3 and a[2][0].isupper() and a[2].isalpha() and a[2][1:].islower():
+                split_score = score.split(':')
+
+                los_angeles_result += int(split_score[0])
+    return los_angeles_result     
