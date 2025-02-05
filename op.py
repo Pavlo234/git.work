7bcc9233979781e856d78981ee002a043e1dd698
Pavlo234 <ahterovp@gmail.com>
2025-02-05 14:53:31 +0200

    repetity

--- op.py
+++ op.py
@@ -12,19 +12,28 @@ def unique_in_order(kl):
         if w != plx[-1]:
             plx.append(w)
     return plx
-
 def unique_in_order(iterable):
     res = []
     for item in iterable:
         if len(res) == 0 or item != res[-1]:
             res.append(item)
     return res
-            
-
-
+      
 p = 'AAAsdddddaaA'
-
-
-
 print(p)
 print(unique_in_order(p))
+
+
+
+def unique_in_order(kl):
+    if len(kl) == 0 :
+        return []
+    # p = kl
+    # kl = []
+    # for i in p:
+    #     kl.append(i)
+
+["Los Angels wdd", "550:511"]
+["LosAngeles", "527:520"]
+["Los Angeles Atomic ", "494:458"]
+["Los Angeles", "469:460"]
